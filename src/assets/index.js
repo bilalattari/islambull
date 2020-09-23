439da9eb34e70c28f00b49c97c5afc56da46636f
Muhammad-Yasir1 <muhammadyasir11000@gmail.com>
2020-09-23 15:02:56 +0500

    unused image removed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,11 @@
 
 import Add from './images/add.png';
-import { Award } from './images/award.png';
 import Background from './images/background.png';
 import Infoicon from './images/infoicon.png';
 import Leftarrow from './images/leftarrow.png';
 import Logo from './images/logo.png';
 import Menuicon from './images/menuicon.png';
 import Musicicon from './images/musicicon.png';
-import Quizimage from './images/quizimage.png';
 import Rightarrow from './images/rightarrow.png';
 import Scorebackground from './images/scorebackground.png';
 import Searchicon from './images/searchicon.png';
@@ -16,14 +14,12 @@ import Userimage from './images/userimage.png';
 
 export {
     Add,
-    Award,
     Background,
     Infoicon,
     Leftarrow,
     Logo,
     Menuicon,
     Musicicon,
-    Quizimage,
     Rightarrow,
     Scorebackground,
     Searchicon,
